f80e7c4b358a1f4aa6da1e55d7b6f8d97e4ec4b6
master <starodubcev@gmail.com>
2026-05-28 05:13:41 +0000

    chore: add gitignore, remove target

--- target/debug/deps/tape-3827c90c070efa99.d
+++
@@ -1,6 +0,0 @@
-/Users/master/cyber/tape/target/debug/deps/tape-3827c90c070efa99.d: src/lib.rs src/molecule.rs
-
-/Users/master/cyber/tape/target/debug/deps/tape-3827c90c070efa99: src/lib.rs src/molecule.rs
-
-src/lib.rs:
-src/molecule.rs:
--- target/debug/deps/tape-5b2b819a5b1bf3be.d
+++
@@ -1,8 +0,0 @@
-/Users/master/cyber/tape/target/debug/deps/tape-5b2b819a5b1bf3be.d: src/lib.rs src/molecule.rs
-
-/Users/master/cyber/tape/target/debug/deps/libtape-5b2b819a5b1bf3be.rlib: src/lib.rs src/molecule.rs
-
-/Users/master/cyber/tape/target/debug/deps/libtape-5b2b819a5b1bf3be.rmeta: src/lib.rs src/molecule.rs
-
-src/lib.rs:
-src/molecule.rs:
